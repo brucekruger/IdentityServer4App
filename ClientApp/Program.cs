dbb1c844dd590a7848b5af3d12c679465d637a54
baranau_an <andrei_baranau1@epam.com>
2021-04-10 22:19:38 +0300

    Added project to repo.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            // discover endpoints from the metadata by calling Auth server hosted on 5000 port
+            var client = new HttpClient();
+
+            var discoveryDocumentResponse = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (discoveryDocumentResponse.IsError)
+            {
+                Console.WriteLine(discoveryDocumentResponse.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discoveryDocumentResponse.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
---
+++ ServerApp/Config.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace ServerApp
+{
+    public class Config
+    {
+        // clients that are allowed to access resources from the Auth server
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+
+        // API that are allowed to access the Auth server
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        // Scopes that are allowed to access the Auth server
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope(name: "api1",   displayName: "Access API Backend")
+            };
+        }
+    }
+}
